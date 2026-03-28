42169cde22dd1d166b06c8984ee131e154c5f6c3
AimanShariff-g-commits <1at24cg006@atria.edu>
2026-03-28 21:57:29 +0530

    updated login UI

--- attendance.js
+++ attendance.js
@@ -308,3 +308,7 @@ window.onload = function () {
 
     document.getElementById("numClasses")?.addEventListener("input", updateTimeRange)
 }
+
+function viewAttendance() {
+    window.location.href = "edit-attendance.html"
+}
